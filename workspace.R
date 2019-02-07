81620db1032f645f4993817a0d7877803728fe93
Thomas Townsley <thomasdtownsley@gmail.com>
2019-02-07 14:58:18 -0600

    added rds files

--- workspace.R
+++ workspace.R
@@ -6,6 +6,9 @@ library(alphavantager)
 library(ggplot2)
 library(corrplot)
 library(twitteR)
+library(dplyr)
+
+## cool repo to look at later: https://github.com/lornamariak/TextMinningR
 
 #Removes strings as factors 
 options(stringsAsFactors = FALSE)
@@ -13,6 +16,14 @@ options(stringsAsFactors = FALSE)
 ### Set API key obtained from site: https://www.alphavantage.co/support/#api-key ###
 
 av_api_key("PCRN5TC83BHHF76D")
+
+#time series data pull example
+
+SPX <- av_get(symbol = "SPX", av_fun = "TIME_SERIES_INTRADAY", interval = "15min")
+RUT <- av_get(symbol = "RUT", av_fun = "TIME_SERIES_INTRADAY", interval = "15min")
+VIX <- av_get(symbol = "VIX", av_fun = "TIME_SERIES_DAILY", outputsize="full")
+
+
 
 ### Twitter API Feed Setup ###
 ## load credentials ##
@@ -26,13 +37,21 @@ access_secret <- "uBaxEzoOzUBtnmku2zYXAyNjfKRboBmLEsnuoUE1aDNjP"
 
 setup_twitter_oauth(consumer_key ,consumer_secret,access_token ,access_secret)
 
-#time series data pull example
 
-SPX <- av_get(symbol = "SPX", av_fun = "TIME_SERIES_INTRADAY", interval = "15min")
-RUT <- av_get(symbol = "RUT", av_fun = "TIME_SERIES_INTRADAY", interval = "15min")
-VIX <- av_get(symbol = "VIX", av_fun = "TIME_SERIES_DAILY", outputsize="full")
+#fetch tweets associated with that hashtag , 12 tweets-n in 
+#(en)glish-lang since the indicated date yy/mm/dd
+tweets <- twitteR::searchTwitter("#$MSFT",n =12, lang="en", since = '2018-01-01')
 
+#strip retweets
+strip_retweets(tweets)
 
+#convert to data frame using the twListtoDF function
+df <- twListToDF(tweets) #extract the data frame save it locally
+saveRDS(df, file = "tweets.rds")
+df1 <- readRDS("mytweets.rds") #Look at RDS file opening?                                
+
+#remove duplicates
+dplyr::distinct(df1)
 
 
 
